7ff18b5027da2cde11134f80d0b87b2824b0b299
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-12-13 16:24:09 -0600

    Create 02_generaete_install_token.sh

---
+++ bash/02_generaete_install_token.sh
@@ -0,0 +1,16 @@
+# Generates a ghs_* Token. GitHub server-to-server tokens.
+# https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-an-installation-access-token-for-a-github-app
+# Usage:
+# bash run.sh <APP_ID> <JWT>
+# Time Limit 1 Hour
+
+#!/usr/bin/env bash
+
+app_id=$1
+jwt=$2
+
+curl --request POST \
+--url "https://api.github.com/app/installations/$1/access_tokens" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer $2" \
+--header "X-GitHub-Api-Version: 2022-11-28"
