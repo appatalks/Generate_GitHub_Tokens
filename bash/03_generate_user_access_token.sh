3bf39f0ed6e9f625664136e05a1ec72e21932b24
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-12-13 16:27:35 -0600

    Create 03_generate_user_access_token.sh

---
+++ bash/03_generate_user_access_token.sh
@@ -0,0 +1,24 @@
+# NOTE: User must first authorize App and grab "code=<CODE_STRING>" from redirect URL.
+# https://github.com/login/oauth/authorize?client_id=<APP_CLIENT_ID>
+# Time Limit a minute or so
+
+# Generates a ghu_* User-to-Server Token.
+# Time Limit 8 Hours
+
+# Generates a ghr_* Refresh Token
+# Time Limit 6 Months
+
+# Usage:
+# bash run.sh <CLIENT_ID> <CLIENT_SECRET> <CODE>
+
+#!/bin/bash
+
+CLIENT_ID=$1
+CLIENT_SECRET=$2
+CODE=$3
+
+# Use curl to make the POST request
+curl -X POST \
+  -H "Accept: application/json" \
+  -d "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$CODE" \
+  https://github.com/login/oauth/access_token
