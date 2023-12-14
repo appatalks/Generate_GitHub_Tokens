7deb94932ae04e0fd81a2d6d78475fddc3e363e7
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-12-14 16:43:43 -0600

    Update 05_generate_oAuth_app_token.sh

--- bash/05_generate_oAuth_app_token.sh
+++ bash/05_generate_oAuth_app_token.sh
@@ -1,7 +1,7 @@
 # https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authenticating-to-the-rest-api-with-an-oauth-app
 # https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
 
-# 1. Create the app at the org grabbing the ID and Secret
+# 1. Create oAuth App grabbing the ID and Secret
 # 2. Run the script, interactively choose what scopes you want
 # 3. copy the code=<string>, paste the code string, grab gho_*
 # 4. Profit
